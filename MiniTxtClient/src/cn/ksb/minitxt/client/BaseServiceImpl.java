686e97e70b440cd12285d12912b55a35ded77404
0ZAWSZE0 <381609508@qq.com>
2018-12-01 18:10:29 +0800

    客户端

---
+++ MiniTxtClient/src/cn/ksb/minitxt/client/BaseServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.ksb.minitxt.client;
+
+import java.io.Serializable;
+
+public abstract class BaseServiceImpl<T extends Serializable> implements Service<T> {
+	protected String OUTPUT_TEXT_SERVERERROR = "服务器故障，请重试！";
+	protected String OUTPUT_TEXT_ERROR = "系统存在错误，服务终止！";
+	protected String OUTPUT_TEXT_INVALIDINPUT = "你的输入有误，请重新输入！";
+	private T inputData;
+
+	@Override
+	public void setInputData(T inputData) {
+		this.inputData = inputData;
+	}
+
+	public T getInputData() {
+		return inputData;
+	}
+
+}
---
+++ MiniTxtClient/src/cn/ksb/minitxt/client/ServiceFactory.java
@@ -0,0 +1,62 @@
+package cn.ksb.minitxt.client;
+
+import java.io.FileInputStream;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ServiceFactory {
+	private static final String path = Init.getProperty("client.config.service");
+	private static Map<String, String> services = new HashMap<String, String>();
+	
+	static {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = null;
+		Document doc = null;
+		
+		try {
+			db = dbf.newDocumentBuilder();
+			doc = db.parse(new FileInputStream(path));
+			NodeList serviceNodes = doc.getElementsByTagName("service");
+			for (int i = 0; i < serviceNodes.getLength(); i++) {
+				Node node = serviceNodes.item(i);
+				services.put(node.getAttributes().getNamedItem("key").getNodeValue().trim(),
+						node.getFirstChild().getNodeValue().trim());
+				
+			}
+		} catch (Exception e) {
+ 			e.printStackTrace();
+ 			throw new RuntimeException(e);
+		} 
+	}
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	public static <T extends Serializable> Service<T> getService(String key) {
+		String className = services.get(key);
+		if(className == null)
+			throw new RuntimeException("ÎÞÐ§¹Ø¼ü×Ö");
+		
+		try {
+			return (Service<T>) Class.forName(className).newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
+	}
+	
+	
+}
+
